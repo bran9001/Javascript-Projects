2a05ba43f88e49020a19605674f4f6ee055ccb94
Brandon <97011703+bran9001@users.noreply.github.com>
2023-02-27 15:39:10 -0700

    Added assignments

---
+++ Project1_expressions_alert/js/main.js
@@ -0,0 +1,15 @@
+var A = "Hello, world!";
+window.alert(A);
+
+document.write("Lisa told bart, \"Knock it off Bart! Or I'll tell dad!\".");
+
+document.write( "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua." 
++ "Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat");
+
+var B = "Concatenated" + " String"
+document.write(B);
+
+var Family = "The Arezzinis", Dad = "Jeremiah", Mom = "Hermoine", Daughter = "Penny", Son = "Zorro";
+document.write(Dad);
+
+Document.write(3 + 3);
