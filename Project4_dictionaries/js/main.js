73e531a17991ac483a3ee7d6c80b935fc02a3d29
Brandon <97011703+bran9001@users.noreply.github.com>
2023-02-27 22:03:29 -0700

    Added delete function

--- Project4_dictionaries/js/main.js
+++ Project4_dictionaries/js/main.js
@@ -13,10 +13,25 @@ for (var key in dictionary) {
 
 
 function lookup() {
-    var word = document.getElementById("word").value.toLowerCase(); // converts value to lowercase
+    var word = document.getElementById("lookup").value.toLowerCase(); // converts value to lowercase
     if (dictionary.hasOwnProperty(word)) {
-      document.getElementById("Dictionary").innerHTML = dictionary[word];
+      document.getElementById("dictionaryLookup").innerHTML = dictionary[word];
     } else {
-      document.getElementById("Dictionary").innerHTML = "Sorry, the word you entered is not in the dictionary.";
+      document.getElementById("dictionaryLookup").innerHTML = "Sorry, the word you entered is not in the dictionary.";
     }
+}
+
+function del() {
+  var word = document.getElementById("del").value.toLowerCase(); // converts value to lowercase
+  if (dictionary.hasOwnProperty(word)) {
+    delete dictionary[word];
+    document.getElementById("dictionaryDel").innerHTML = word + " has been deleted";
+    document.getElementById("kvp").innerHTML = '';
+    var testDiv = document.getElementById("kvp");
+    for (var key in dictionary) {
+      testDiv.innerHTML += key + ": " + dictionary[key] + "<br>";
+    }
+  } else {
+    document.getElementById("dictionaryDel").innerHTML = "Sorry, the word you entered is not in the dictionary.";
+  }
 }
